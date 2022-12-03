4713ad67bb83dc3e76702cc802550fcbb6f3f753
Jacquese <jacqwhitson@gmail.com>
2022-12-02 23:51:43 -0500

    Initial Commit

---
+++ StanfordProject/StanfordProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordProjectApp.swift
+//  StanfordProject
+//
+//  Created by Jacquese Whitson  on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct StanfordProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
